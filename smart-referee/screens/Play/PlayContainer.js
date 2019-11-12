48a0252833c5dec1d952e8aa1d6c4c8c3dc359f8
김민수 <alstn2468_@naver.com>
2019-11-12 23:15:58 +0900

    Fix splash image mode contain -> cover

--- smart-referee/screens/Play/PlayContainer.js
+++ smart-referee/screens/Play/PlayContainer.js
@@ -5,158 +5,150 @@ import PlayPresenter from "./PlayPresenter";
 import * as Permissions from "expo-permissions";
 import * as ImageManipulator from "expo-image-manipulator";
 import {
-    captureRef as takeSnapshotAsync,
-    captureScreen
+  captureRef as takeSnapshotAsync,
+  captureScreen
 } from "react-native-view-shot";
 import * as MediaLibrary from "expo-media-library";
 
 let check = 1;
 
 export default class extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loading: false,
-            isRecord: false,
-            hasPermission: null,
-            video: null
-        };
+    this.state = {
+      loading: false,
+      isRecord: false,
+      hasPermission: null,
+      video: null
+    };
 
-        this.cameraRef = React.createRef();
+    this.cameraRef = React.createRef();
+  }
+
+  componentDidMount = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    if (status === "granted") {
+      this.setState({ hasPermission: true });
+    } else {
+      this.setState({ hasPermission: false });
     }
+  };
 
-    componentDidMount = async () => {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+  componentWillUnmount = () => {
+    clearTimeout(this.takePhotoRecursion);
+  };
 
-        if (status === "granted") {
-            this.setState({ hasPermission: true });
-        } else {
-            this.setState({ hasPermission: false });
-        }
-    };
+  _StartPauseButtonClicked = () => {
+    const { isRecord } = this.state;
 
-    componentWillUnmount = () => {
-        clearTimeout(this.takePhotoRecursion);
-    };
-
-    _StartPauseButtonClicked = () => {
-        const { isRecord } = this.state;
-
-        if (isRecord) {
-            // this._StopRecordingVideo();
-            clearTimeout(this.takePhotoRecursion);
-            this.setState({ isRecord: false });
-        } else {
-            // this._StartRecordingVideo();
-            this._TakePhoto();
-            this.setState({ isRecord: true });
-        }
-    };
-
-    _SavePhoto = async uri => {
-        const ALBUM_NAME = "SMART REFEREE";
-
-        try {
-            const { status } = await Permissions.askAsync(
-                Permissions.CAMERA_ROLL
-            );
-            if (status === "granted") {
-                const asset = await MediaLibrary.createAssetAsync(uri);
-                let album = await MediaLibrary.getAlbumAsync(ALBUM_NAME);
-                if (album === null) {
-                    album = await MediaLibrary.createAlbumAsync(
-                        ALBUM_NAME,
-                        asset
-                    );
-                } else {
-                    await MediaLibrary.addAssetsToAlbumAsync([asset], album.id);
-                }
-            } else {
-                this.setState({ hasPermission: false });
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    _TakePhoto = async () => {
-        try {
-            if (this.cameraRef.current) {
-                // let captureResult = await takeSnapshotAsync(
-                //     this.cameraRef.current,
-                //     {
-                //         format: "jpg",
-                //         width: 416,
-                //         height: 416,
-                //         quality: 0.1
-                //     }
-                // );
-
-                let captureResult = await captureScreen({
-                    format: "jpg",
-                    quality: 0.1
-                });
-
-                let image = null;
-
-                this._SavePhoto(captureResult);
-
-                console.log(check++);
-
-                // if (Platform.OS === "android") {
-                //     let resizedImage = await ImageManipulator.manipulateAsync(
-                //         captureResult,
-                //         [
-                //             { resize: { width: 416, height: 416 } },
-                //             { rotate: 270 }
-                //         ],
-                //         { format: ImageManipulator.SaveFormat.JPEG }
-                //     );
-
-                //     image = resizedImage.uri;
-                // } else if (Platform.OS === "ios") {
-                //     throw Error("iOS에서는 이용할 수 없습니다.");
-                //     image = captureResult;
-                // }
-
-                // let formData = new FormData();
-
-                // let date = Date.now();
-
-                // formData.append(image, {
-                //     uri: image,
-                //     type: "image/jpg",
-                //     name: `image-${date}.jpg`
-                // });
-
-                // const data = await imageUploadApi.uploadImage(formData);
-                // console.log(data);
-
-                this.takePhotoRecursion = setTimeout(
-                    () => this._TakePhoto(),
-                    30
-                );
-            }
-        } catch (error) {
-            Alert.alert("Error Corrupt");
-            console.log(error);
-        }
-    };
-
-    render() {
-        const { hasPermission, loading, isRecord } = this.state;
-
-        return (
-            <PlayPresenter
-                loading={loading}
-                hasPermission={hasPermission}
-                isRecord={isRecord}
-                _StartPauseButtonClicked={this._StartPauseButtonClicked}
-                cameraRef={this.cameraRef}
-            />
-        );
+    if (isRecord) {
+      // this._StopRecordingVideo();
+      clearTimeout(this.takePhotoRecursion);
+      this.setState({ isRecord: false });
+    } else {
+      // this._StartRecordingVideo();
+      this._TakePhoto();
+      this.setState({ isRecord: true });
     }
+  };
+
+  _SavePhoto = async uri => {
+    const ALBUM_NAME = "SMART REFEREE";
+
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status === "granted") {
+        const asset = await MediaLibrary.createAssetAsync(uri);
+        let album = await MediaLibrary.getAlbumAsync(ALBUM_NAME);
+        if (album === null) {
+          album = await MediaLibrary.createAlbumAsync(ALBUM_NAME, asset);
+        } else {
+          await MediaLibrary.addAssetsToAlbumAsync([asset], album.id);
+        }
+      } else {
+        this.setState({ hasPermission: false });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  _TakePhoto = async () => {
+    try {
+      if (this.cameraRef.current) {
+        // let captureResult = await takeSnapshotAsync(
+        //     this.cameraRef.current,
+        //     {
+        //         format: "jpg",
+        //         width: 416,
+        //         height: 416,
+        //         quality: 0.1
+        //     }
+        // );
+
+        let captureResult = await captureScreen({
+          format: "jpg",
+          quality: 0.1
+        });
+
+        let image = null;
+
+        this._SavePhoto(captureResult);
+
+        console.log(check++);
+
+        // if (Platform.OS === "android") {
+        //     let resizedImage = await ImageManipulator.manipulateAsync(
+        //         captureResult,
+        //         [
+        //             { resize: { width: 416, height: 416 } },
+        //             { rotate: 270 }
+        //         ],
+        //         { format: ImageManipulator.SaveFormat.JPEG }
+        //     );
+
+        //     image = resizedImage.uri;
+        // } else if (Platform.OS === "ios") {
+        //     throw Error("iOS에서는 이용할 수 없습니다.");
+        //     image = captureResult;
+        // }
+
+        // let formData = new FormData();
+
+        // let date = Date.now();
+
+        // formData.append(image, {
+        //     uri: image,
+        //     type: "image/jpg",
+        //     name: `image-${date}.jpg`
+        // });
+
+        // const data = await imageUploadApi.uploadImage(formData);
+        // console.log(data);
+
+        this.takePhotoRecursion = setTimeout(() => this._TakePhoto(), 30);
+      }
+    } catch (error) {
+      Alert.alert("Error Corrupt");
+      console.log(error);
+    }
+  };
+
+  render() {
+    const { hasPermission, loading, isRecord } = this.state;
+
+    return (
+      <PlayPresenter
+        loading={loading}
+        hasPermission={hasPermission}
+        isRecord={isRecord}
+        _StartPauseButtonClicked={this._StartPauseButtonClicked}
+        cameraRef={this.cameraRef}
+      />
+    );
+  }
 }
 
 // _StartRecordingVideo = async () => {
